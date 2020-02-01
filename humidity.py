4709a36f185335d1cb13d70ec8124320a69348a5
Andy Antes <aantes@tableau.com>
2020-02-01 10:40:13 -0800

    Changed to use different library

--- humidity.py
+++ humidity.py
@@ -1,12 +1,21 @@
-import Adafruit_DHT
+import time
+import board
+import adafruit_dht
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D18)
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
+              .format(temperature_f, temperature_c, humidity))
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor")
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+
+    time.sleep(2.0)
