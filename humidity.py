590aebcda3c02b36564e2ef45e1e9cf0c5bc4cb5
Andy Antes <aantes@tableau.com>
2020-02-01 12:09:08 -0800

    Added room environment variable

--- humidity.py
+++ humidity.py
@@ -1,8 +1,10 @@
 import time
+import os
 import Adafruit_DHT
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+ROOM = os.getenv("ROOM")
 
 def to_fahrenheit(temp):
     return (temp * 9/5) + 32
@@ -12,7 +14,8 @@ while True:
 
     if humidity is not None and temperature is not None:
         temp_f = to_fahrenheit(temperature)
-        print("Temp={0:0.1f}*F  Humidity={1:0.1f}%".format(temp_f, humidity))
+        print("Room: {} Temp={0:0.1f}*F  Humidity={1:0.1f}%"
+            .format(ROOM, temp_f, humidity))
     else:
         print("Failed to retrieve data from humidity sensor")
 
