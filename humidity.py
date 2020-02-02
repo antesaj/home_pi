b4b4eb0ee36e70e5ce874246508d2c9359cdab5e
Andy Antes <aantes@tableau.com>
2020-02-01 16:14:09 -0800

    Changed kafka producer code

--- humidity.py
+++ humidity.py
@@ -9,11 +9,11 @@ DHT_PIN = 4
 ROOM = os.getenv("ROOM")
 KAFKA = '10.0.0.111:9092'
 
-def publish_message(producer_instance, topic_name, key, value):
+def publish_message(producer_instance, topic_name, data):
     try:
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
-        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+        producer_instance.send(topic_name, data=value_bytes)
         producer_instance.flush()
         print("Message published successfully")
     except Exception as ex:
@@ -23,7 +23,8 @@ def publish_message(producer_instance, topic_name, key, value):
 def connect_kafka_producer():
     _producer = None
     try:
-        _producer = KafkaProducer(bootstrap_servers=[KAFKA], api_version=(0, 10))
+        _producer = KafkaProducer(bootstrap_servers=[KAFKA],
+                                  value_serializer=lambda x: dumps(x).encode('utf-8'))
     except Exception as ex:
         print("Failed to connect to Kafka")
         print(str(ex))
@@ -42,7 +43,8 @@ while True:
         temp_f = to_fahrenheit(temperature)
         temp_str = "Room: {0} Temp={1:0.1f}*F  Humidity={2:0.1f}%".format(ROOM, temp_f, humidity)
         print(temp_str)
-        publish_message(kafka_producer, 'office-temp', 'raw', temp_str)
+        data = {'temp': temp_f, 'humidity': humidity}
+        publish_message(kafka_producer, 'office-temp', data)
     else:
         print("Failed to retrieve data from humidity sensor")
 
